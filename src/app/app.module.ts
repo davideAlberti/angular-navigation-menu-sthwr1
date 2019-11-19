a30688a3d02379927c474c3a8047e0a98725777d
davideAlberti <davide.alberti@marconirovereto.it>
2019-11-19 22:40:40 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { RouterModule } from '@angular/router';
+import { PagineModule } from './pagine/pagine.module';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule, 
     MDBBootstrapModule.forRoot(),
-    RouterModule
+
+    PagineModule
   ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
